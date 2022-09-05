0c7f3f052719cab14dd7b3db9272fc55f13b2416
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-09-05 21:37:50 +0530

    modified the file

--- ch_05/ch_05-part_01-timestamp_03:00.py
+++ ch_05/ch_05-part_01-timestamp_03:00.py
@@ -4,3 +4,6 @@ while n>0:
     n = n-1
 print('Blastoff!')
 print(n)
+
+# loops (repeated steps) have iteration variables that change each time through a loop.
+# Often these iteration variables go through a sequence of numbers.
