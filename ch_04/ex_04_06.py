df58a6d17c21328bb213e72c1320a90bf8fd1915
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-09-05 10:47:49 +0530

    completed exercise 4.6

---
+++ ch_04/ex_04_06.py
@@ -0,0 +1,19 @@
+def computepay(hours, rate):
+    #print("In computepay", hours, rate)
+    if hours > 40 :
+        reg = rate * hours
+        otp = (hours-40.0) * (rate * 0.5)
+        pay = reg + otp
+    else:
+        pay = hours * rate
+        #print("Returning", pay)
+    return pay
+
+hrs = input("Enter Hours:")
+rate = input("Enter rate:")
+fh = float(hrs)
+fr = float(rate)
+
+xp = computepay(fh,fr)
+
+print("Pay", xp)
