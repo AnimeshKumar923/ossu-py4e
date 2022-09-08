0a8ebf9af6e9e70a9b861996a1be977e9547b8a0
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-09-08 09:00:16 +0530

    created a new program

---
+++ ch_05/ch_05-part_02-timestamp_01:45.py
@@ -0,0 +1,9 @@
+for i in [5,4,3,2,1]:
+    print(i)
+print('Blastoff!')
+
+# for is a definite loop.
+
+# a definite loop is a loop in which the number of times it is going to execute is known in advance before entering the loop. 
+
+# indefinite loop is that which is executed until a certain condition is met. The number of times of execution is not known in advance.
