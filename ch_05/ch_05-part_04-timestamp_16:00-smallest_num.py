c455b8dbfb25f61cf890c880bbfc9007bf023c1f
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-09-11 11:46:44 +0530

    modification done in file

--- ch_05/ch_05-part_04-timestamp_16:00-smallest_num.py
+++ ch_05/ch_05-part_04-timestamp_16:00-smallest_num.py
@@ -7,3 +7,5 @@ for value in [10,41,12,5,3,1,45,678,]:
         smallest = value
     print(smallest, value)
 print('After', smallest)
+
+# "is" and "is not" are powerful operators that "==" but recommended to use it only in boolean (T/F) and in "None" function.
