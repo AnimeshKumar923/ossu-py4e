86f1d7aab5cba54ad38de0bb7e4ce3cc494843c4
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-09-11 11:19:04 +0530

    modified the code of previous file

---
+++ ch_05/ch_05-part_04-timestamp_07:54-boolean_search_modified.py
@@ -0,0 +1,9 @@
+found = False
+print('Before', found)
+for value in [34,56,32,4,7,9,98,0]:
+    if value == 9:
+        found = True
+    else:
+        found = False
+    print(found, value)
+print('After', found)
