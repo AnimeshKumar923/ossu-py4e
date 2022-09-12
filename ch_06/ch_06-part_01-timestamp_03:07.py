293a0284f0b3046a3102b43877523bc24fa378e4
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-09-12 08:26:16 +0530

    modified the file

--- ch_06/ch_06-part_01-timestamp_03:07.py
+++ ch_06/ch_06-part_01-timestamp_03:07.py
@@ -1,3 +1,12 @@
+from tkinter import Y
+
+
 fruit = 'banana'
 letter = fruit[1]
 print(letter)
+
+# we can get at any single character in a string using an index specified in square brackets.
+
+# the index value must be an integer and starts at zero
+
+# the index value can be and expression that is computed
