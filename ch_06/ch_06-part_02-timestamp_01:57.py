982a989396fc01c151b02942e181a48d6004b86f
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-09-13 21:39:03 +0530

    modified the file

--- ch_06/ch_06-part_02-timestamp_01:57.py
+++ ch_06/ch_06-part_02-timestamp_01:57.py
@@ -2,3 +2,5 @@ s = 'Monty Python'
 print(s[:2])
 print(s[8:])
 print(s[:])
+
+# if we leave off the first number or the last number of the slice, it is assumed to be the beginning or end of the string respectively
