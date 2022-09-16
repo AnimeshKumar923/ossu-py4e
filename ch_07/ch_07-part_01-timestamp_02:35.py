2336775674e7e0d3723e34c2998238304a544ac6
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-09-16 08:03:21 +0530

    started a new chapter

---
+++ ch_07/ch_07-part_01-timestamp_02:35.py
@@ -0,0 +1,9 @@
+# a text file can be thought of as a sequence of lines
+
+# handle = open(filename, mode)
+
+# r-> file opens in read mode
+
+# w-> file opens in write mode
+
+handle = open('demo-file.txt', 'r)
