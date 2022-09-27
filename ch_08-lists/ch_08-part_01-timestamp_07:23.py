3983e2219bbe640f128cedea414550ca5fc853cc
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-09-27 22:41:06 +0530

    modified one file, created new program

--- ch_08-lists/ch_08-part_01-timestamp_07:23.py
+++ ch_08-lists/ch_08-part_01-timestamp_07:23.py
@@ -1,2 +1,8 @@
 lotto = [2,14,2658,568,87,99]
 print(lotto)
+
+# string are immutable, i.e., they cannot be changed by code. They remain as they were in the initial assigned state.
+
+# lists are mutabe, i.e., they can be changed by writing code with replacement of an element being an example of it. Index operator is used to change/replace the element.
+
+
