5e308ba9a12764bc4e5f7d5404abf6ed6393f146
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-10-27 21:52:53 +0530

    new program

---
+++ ch_08-lists/ch_08-part_02-timestamp_05:17.py
@@ -0,0 +1,11 @@
+# a list can hold many items and keeps those items in the order until we do something to change the order.
+
+# a list can be sorted (i.e., change its order)
+
+# the sort method (unlike in strings) means "sort yourself"
+
+friends = ['Joseph', 'George', 'Vandana']
+friends.sort()
+print(friends)
+
+print(friends[2])
