75877c27cb74d2226a19baa6c8f6c6eaa90425ba
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-10-27 21:36:07 +0530

    new program

--- ch_08-lists/ch_08-part_02-timestamp_03:40.py
+++ ch_08-lists/ch_08-part_02-timestamp_03:40.py
@@ -1,3 +1,8 @@
+# we can create an empty list and then add elements using the append method.
+
+# the list stays in order and new elements are added at the end of the list.
+
+
 stuff = list()
 stuff.append('book')
 stuff.append(99)
