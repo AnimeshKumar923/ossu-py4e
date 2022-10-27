38a7d825974be72cc22cc62ed4bfc502a6ee7ec2
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-10-27 21:43:47 +0530

    new program

--- ch_08-lists/ch_08-part_02-timestamp_03:40.py
+++ ch_08-lists/ch_08-part_02-timestamp_03:40.py
@@ -1,3 +1,5 @@
+# Building a list from scratch
+
 # we can create an empty list and then add elements using the append method.
 
 # the list stays in order and new elements are added at the end of the list.
---
+++ ch_08-lists/ch_08-part_02-timestamp_04:11.py
@@ -0,0 +1,12 @@
+# python provides two operators that let you check if an item is in a list.
+
+# these are logical operators that return 'True' or 'False'.
+
+# they don't modify the list
+
+some = [1, 8 ,748, 65, 45, 65]
+#9 in some
+
+#748 in some
+
+#55 not in some
