3964829dafecb9991182c9c2c36c7e02f8602e55
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-10-28 21:01:23 +0530

    new file, breaking a string

---
+++ ch_08-lists/ch_08-part_03-timestamp_01:55.py
@@ -0,0 +1,11 @@
+# Split breaks a string into parts and produces a list of strings.
+# We think of these as words. We can access a particular word or loop through all the words.
+
+string = 'Split breaks string'
+
+splitting = string.split()
+print(splitting)
+
+print(len(splitting))
+
+print(splitting[1])
