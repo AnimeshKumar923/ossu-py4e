498809ff7cbb27518f8c89dc1991ed1e93aae1a5
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-11-01 19:00:34 +0530

    started new chapter - dictionaries

---
+++ ch_09-dictonaries/ch_09-part_01-timestamp_04:38.py
@@ -0,0 +1,16 @@
+# List--> a linear collection of values that stay in order.
+
+# Dictionary--> a "bag" of values, each with its own label.
+
+bag = dict()
+
+bag['money'] = 45
+bag['toffee'] = 12
+bag['bookmark'] = 23
+
+print(bag)
+
+print(bag['toffee'])
+
+bag['money'] = bag['money'] + 100
+print(bag)
