1bc971557ee92a8058cb03970c7edf7076a5df96
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-11-01 18:21:42 +0530

    renamed files

--- ch_08-lists/autograder-8.4.py
+++ ch_08-lists/autograder-8.4.py
@@ -1,11 +1,20 @@
 filename = input('Enter file name: ')
-file = open(filename)
 
-li = list()
+if len(filename) < 1:
+    filename = 'mbox-short.txt'
+
+file = open(filename)
+count = 0
 for line in file:
+    line = line.rstrip()
+    if not line.startswith('From'):
+        continue
+    
+    elif line.startswith('From:'):
+        continue
+    
+    count = count + 1
     words = line.split()
-    for each in words:
-        if not each in li:
-            li.append(each)
-li.sort()
-print(li)
+    print(words[1])
+
+print('There were', count, 'lines in the file with From as the first word')
