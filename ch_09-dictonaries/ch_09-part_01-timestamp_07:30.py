efd25185746c4d853fb9ccb53c118db9f5d89888
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-11-01 19:17:24 +0530

    new file, dictionary literals

---
+++ ch_09-dictonaries/ch_09-part_01-timestamp_07:30.py
@@ -0,0 +1,9 @@
+# dictionary literals are curly braces and have a list of key:value pairs
+
+# you can make an empty dictionary using empty curly braces.
+
+jojo = {'chuck':2, 'nice':56, 'joestar':124}
+print(jojo)
+
+luck={}
+print(luck)
