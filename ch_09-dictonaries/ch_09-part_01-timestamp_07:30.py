2c269514321e2e49671321079013e22a7636d6fe
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-11-10 19:27:31 +0530

    looping through dictionaries

--- ch_09-dictonaries/ch_09-part_01-timestamp_07:30.py
+++ ch_09-dictonaries/ch_09-part_01-timestamp_07:30.py
@@ -1,9 +1,9 @@
 # dictionary literals are curly braces and have a list of key:value pairs
 
-# you can make an empty dictionary using empty curly braces.
-
 jojo = {'chuck':2, 'nice':56, 'joestar':124}
 print(jojo)
+
+# you can make an empty dictionary using empty curly braces.
 
 luck={}
 print(luck)
---
+++ ch_09-dictonaries/ch_09-part_03-timestamp_03:00.py
@@ -0,0 +1,6 @@
+# looping through dictionaries
+
+counts = {'chuck': 1, 'fred' : 42, 'jan' : 120}
+
+for key in counts:
+    print(key, counts[key])
